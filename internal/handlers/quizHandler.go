49abdc32c8363e14dab1a41e9e2ea63799873d8f
razvan-bara <rzvbara@gmail.com>
2023-10-23 17:01:48 +0300

    complete error handling

--- internal/handlers/quizHandler.go
+++ internal/handlers/quizHandler.go
@@ -2,8 +2,11 @@ package handlers
 
 import (
 	"VUGO-API/api/quiz_api/squiz"
+	"VUGO-API/api/sdto"
 	"VUGO-API/internal/services"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/swag"
+	"net/http"
 )
 
 type QuizHandler struct {
@@ -19,11 +22,11 @@ func (handler *QuizHandler) ProcessNewQuiz(req squiz.AddQuizParams) middleware.R
 	// add the quiz to db to get id
 	res, err := handler.quizService.ProcessNewQuiz(quiz)
 	if err != nil {
-
+		squiz.NewAddQuizInternalServerError().WithPayload(&sdto.Error{
+			Code:    swag.Int64(http.StatusInternalServerError),
+			Message: swag.String(err.Error()),
+		})
 	}
-	// add each question to db to get id
-	// add each answer to db≠
-	// build response
 
 	return squiz.NewAddQuizCreated().WithPayload(res)
 }
